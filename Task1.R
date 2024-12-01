5e1455fe0431404d08770694a8c07baf75d0ef65
SnaggelsHD <98557361+SnaggelsHD@users.noreply.github.com>
2024-12-01 06:49:16 +0100

    Task1

--- Task1.R
+++ Task1.R
@@ -1,11 +1,28 @@
-df <- read.delim2("Task1.txt", sep =" ", header = FALSE)
+df <- read.delim("./Task1.txt", sep =" ", header = FALSE)
 
-#sort the two collumns
-df$C1 <- sort(df$V1)
-df$C2 <- sort(df$V4)
+# Part 1
+
+# sort the two collumns
+df$V1Sort <- sort(df$V1)
+df$V2Sort <- sort(df$V4)
 
 # calculate the absolute difference between the two items of each row
-df$diff <- abs(df$C1 - df$C2)
+df$diff <- abs(df$V1Sort - df$V2Sort)
 
 # calculate the sum of the differences
-sum(df$diff)
+sumDiff <- sum(df$diff)
+
+
+# Part 2
+
+# search how often left is in right
+
+df$leftInRight <- sapply(df$V1, function(x) sum(df$V4 == x))
+
+# left * times in right
+
+df$leftTimesInRight <- df$V1 * df$leftInRight
+
+# sum the values
+sumLeftTimesInRight <- sum(df$leftTimesInRight)
+
--- Task2.R
+++
@@ -1,7 +0,0 @@
-df <- read.delim2("Task1.txt", sep =" ", header = FALSE)
-
-# how much does each item in the left appear in the right
-df$C1 <- sapply(df$V1, function(x) sum(df$V4 == x))
-
-#sum the values
-sum(df$C1)
